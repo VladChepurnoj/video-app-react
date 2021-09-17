ad151a2653f4080188ddf849a8dbaad8ae9c3d8f
Vlad <chepurnojv@gmail.com>
2021-09-17 19:20:52 +0300

    videolist comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
-export class App extends Component {
-  onTermSubmit = (term) => {
-    youtube.get("/search", {
+class App extends Component {
+  state = { videos: [] };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
+
+    this.setState({ videos: response.data.items });
   };
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
